c1edce2e603a2228664ecbbddc36bfe53f2acd3e
Bitcamp-PC\Bitcamp <sdfd@sdf.daf>
2016-08-19 17:17:00 +0900

    AccountVO

---
+++ Minihompy/src/vo/AccountVO.java
@@ -0,0 +1,23 @@
+package vo;
+/*
+ *  sort_no number primary key,
+	user_no number,
+	category_no number,
+	tran_date date default sysdate not null,
+	payment char(1) not null,
+	description varchar2(30) not null,
+	note varchar2(500) not null,
+	amount number not null
+	
+	
+    category_no number primary key,
+	name varchar2(100) not null
+ */
+
+public class AccountVO {
+	private int sortNo;
+	private String userName;
+	
+	
+
+}
